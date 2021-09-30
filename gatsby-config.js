6242603769b0e9dc91370642ceab0a77e43727ef
Vitor Veras <vitorverasm@gmail.com>
2021-09-30 09:40:51 -0300

    chore: update config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,12 @@
 module.exports = {
   siteMetadata: {
     // edit below
-    title: `Gatsby Starter Personal Blog`,
-    author: `Gatsby`,
+    title: `My Personal Blog 2`,
+    author: `Vitor Veras`,
     description: `A starter personal blog with styled components, dark mode, and Netlify CMS.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    siteUrl: `https://vitorverasmblog.netlify.app`,
     social: {
-      twitter: `gatsbyjs`,
+      twitter: `vitorverasm`,
     },
   },
   plugins: [
